93b10d9b74309584fad4954c2a2f6a2f86ea26eb
Milen Mladenov <milen.mladenov@bbraun.com>
2023-08-17 10:29:22 +0300

    change theme

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -20,7 +20,7 @@ function usePrevious(theme) {
  */
 function useStorageTheme(key) {
   const userPreference =
-    !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+    !!window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches
 
   const [theme, setTheme] = useState(
     // use stored theme; fallback to user preference
@@ -50,7 +50,7 @@ export const ThemeProvider = ({ children }) => {
   }, [theme, oldTheme])
 
   function toggleTheme() {
-    if (theme === 'light') setTheme('dark')
+    if (theme === 'light') setTheme('light')
     else setTheme('light')
   }
 
