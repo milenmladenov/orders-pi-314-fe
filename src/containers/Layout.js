b8fc439b219668d7b45558b32493250727458860
Milen Mladenov <milen.mladenov@bbraun.com>
2023-08-17 11:31:34 +0300

    remove dark theme

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -6,7 +6,6 @@ import adminRoutes from '../adminRoutes'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 import Main from '../containers/Main'
-import ThemedSuspense from '../components/ThemedSuspense'
 import { SidebarContext } from '../context/SidebarContext'
 
 const Page404 = lazy(() => import('../pages/404'))
@@ -30,7 +29,7 @@ function Layout() {
         <div className="flex flex-col flex-1 w-full">
           <Header />
           <Main>
-            <Suspense fallback={<ThemedSuspense />}>
+            <Suspense>
               <Switch>
                 {adminRoutes.map((adminRoute, i) => {
                   return adminRoute.component ? (
@@ -60,7 +59,7 @@ function Layout() {
       <div className="flex flex-col flex-1 w-full">
         <Header />
         <Main>
-          <Suspense fallback={<ThemedSuspense />}>
+          <Suspense>
             <Switch>
               {routes.map((route, i) => {
                 return route.component ? (
