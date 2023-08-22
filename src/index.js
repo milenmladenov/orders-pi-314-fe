18fe1eca36a34b8a2c77d0a4e267e245059c2ddc
Milen Mladenov <milen.mladenov@bbraun.com>
2023-08-22 09:20:35 +0300

    build

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/tailwind.output.css";
+import App from "./App";
+import { SidebarProvider } from "./context/SidebarContext";
+import ThemedSuspense from "./components/ThemedSuspense";
+import { Windmill } from "@windmill/react-ui";
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <SidebarProvider>
+    <Suspense fallback={<ThemedSuspense />}>
+      <Windmill>
+        <App />
+      </Windmill>
+    </Suspense>
+  </SidebarProvider>,
+  document.getElementById("root")
+);
+serviceWorker.register();
